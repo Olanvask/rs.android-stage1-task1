62d5fdbcc6d4d5b4bb95193c6a19c475e23acfc5
MeilyD07 <meilyd07@gmail.com>
2020-03-22 07:40:39 +0300

    feat: add highest palindrom

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -4,6 +4,100 @@ class HighestPalindrome {
 
     // TODO: Complete the following function
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        var min = 0
+        var max = n - 1
+        var difference = 0
+        var characters = digitString.toCharArray()
+        var countSteps = k
+        var i = 0
+        var j = n - 1
+
+        while (i < n / 2) {
+            if (characters[i] != characters[j]) {
+                difference++
+            }
+            i++
+            j--
+        }
+
+        if (difference > countSteps) {
+            return "-1";
+        }
+
+        while (max >= min) {
+            if (countSteps <= 0) {
+                break
+            }
+
+            var minItem = characters[min]
+            var maxItem = characters[max]
+
+            if (minItem == maxItem) {
+                if ((countSteps > 1) && ((countSteps - 2) >= difference) && (minItem != '9')) {
+                    characters[min] = '9'
+                    characters[max] = '9'
+                    countSteps = countSteps - 2
+                }
+            } else {
+                if ((countSteps > 1) && ((countSteps - 2) >= difference - 1)) {
+                    if (minItem != '9') {
+                        characters[min] = '9'
+                        countSteps--
+                    }
+
+                    if (maxItem != '9') {
+                        characters[max] = '9'
+                        countSteps--
+                    }
+                } else {
+                    val minDigitValue = characters[min].toInt()
+                    val maxDigitValue = characters[max].toInt()
+
+                    if (minDigitValue > maxDigitValue) {
+                        characters[max] = characters[min]
+                    } else {
+                        characters[min] = characters[max]
+                    }
+                    countSteps--
+                }
+                difference--
+            }
+
+            if ((max == min) && (countSteps > 0)){
+                characters[min] = '9'
+                countSteps--
+            }
+            min++
+            max--
+        }
+
+        if (this.isPalindrome(characters)) {
+            val result: String = characters.joinToString("");
+            return  result
+        } else {
+            return "-1"
+        }
+    }
+
+    private fun isPalindrome (stringList: CharArray): Boolean {
+        if (stringList.count() == 0) {
+            return false
+        }
+
+        var i = 0
+        var j = stringList.count() - 1
+
+        while (j < i) {
+            val firstChar = stringList[i]
+            val secondChar = stringList[j]
+
+            if (firstChar == secondChar) {
+                i++
+                j--
+            } else {
+                return false
+            }
+        }
+        return true;
     }
 }
