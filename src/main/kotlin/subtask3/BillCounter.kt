4c40b9cd73c1fa84863340f87a59ab31aa564ea5
MeilyD07 <meilyd07@gmail.com>
2020-03-22 03:31:07 +0300

    feat: add bill counter

--- src/main/kotlin/subtask3/BillCounter.kt
+++ src/main/kotlin/subtask3/BillCounter.kt
@@ -5,6 +5,21 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+
+        val count = bill.count()
+        if ((count > 1) && (k < count) && (k >= 0)) {
+            var totalSum = bill.sum()
+            totalSum = totalSum - bill[k]
+
+            val amount: Int = totalSum / 2
+
+            if (b == amount) {
+                return "bon appetit"
+            } else {
+                return (b - amount).toString()
+            }
+        }
+
+        return ""
     }
 }
